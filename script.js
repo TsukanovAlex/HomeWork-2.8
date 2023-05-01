8a019666d719ed28818a42cdb57d06628084522d
*Aleksey <*atsukanov89@gmail.com*>
2023-05-01 15:59:21 +0300

    Выполнено задание № 2

--- script.js
+++ script.js
@@ -21,3 +21,34 @@ const people1 = [
 ];
 console.log(people1.sort((a,b) => a.age - b.age));
 
+// Задание № 2
+
+function isPositive(a) {
+  return a >= 0;
+  }
+
+function isMale(people) {
+    return people.gender === 'male';
+  }
+
+function filter(arr, myFunction) {
+    const resultArr = [];
+    
+    for (let i = 0; i < arr.length; i++){
+    if (myFunction(arr[i])) {
+      resultArr.push(arr[i]);
+    }
+  }
+  return resultArr;
+  }
+  
+console.log(filter([3, -4, 1, 9], isPositive));
+  
+const people = [
+     {name: 'Глеб', gender: 'male'},
+     {name: 'Анна', gender: 'female'},
+     {name: 'Олег', gender: 'male'},
+     {name: 'Оксана', gender: 'female'}
+  ];
+  
+  console.log(filter(people, isMale)); 
